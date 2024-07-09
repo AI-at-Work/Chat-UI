7fb2e008bcc1d79c52da0e62bd27e5d2ed20aa2a
mr-shitij <21.shitijagrawal@gmail.com>
2024-07-10 00:03:53 +0530

    UI Done

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,5 +1,5 @@
 export const WEBSOCKET_URL = 'ws://localhost:8000/ws';
-export const USER_ID = '5b8a5456-0fa0-41d1-9e2f-241d8da952d9';
+export const USER_ID = '9d1de1c2-bfd9-4f32-9791-a97aeb2d25e2';
 export const USERNAME = 'Default';
 export const MODEL_ID     = 4;
 export const SESSION_PROMPT = "";
